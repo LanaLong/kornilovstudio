d830200cbf2bc243f5edf206433e5f8b6a5e3a4c
LanaLong <qmexperience@gmail.com>
2020-06-12 19:33:22 +0300

    add cover video and header styles

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,17 +1,17 @@
 import './index.css';
-import '../../../node_modules/isotope-layout/dist/isotope.pkgd.min.js';
-import '../../js/utils/isotopHelper';
+// import '../../../node_modules/isotope-layout/dist/isotope.pkgd.min.js';
+// import '../../js/utils/isotopHelper';
 
 // FROM https://isotope.metafizzy.co/#install
-const elem = document.querySelector('.grid');
-const iso = new Isotope( elem, {
-    // options
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows'
-});
-
-// element argument can be a selector string
-//   for an individual element
-const iso = new Isotope( '.grid', {
-    // options
-});
+// const elem = document.querySelector('.grid');
+// const iso = new Isotope( elem, {
+//     // options
+//     itemSelector: '.grid-item',
+//     layoutMode: 'fitRows'
+// });
+//
+// // element argument can be a selector string
+// //   for an individual element
+// const iso = new Isotope( '.grid', {
+//     // options
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(png|jpg|gif|ico|svg)$/,
+                test: /\.(png|jpg|gif|ico|svg|mp4)$/,
                 use: [
                     'file-loader?name=./images/[name].[ext]',
                     {
