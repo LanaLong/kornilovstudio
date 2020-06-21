6699764d73125695ad6cab50df6c4315ee6e6cd6
LanaLong <qmexperience@gmail.com>
2020-06-21 16:06:20 +0300

    gallery try01

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,15 @@
 import './index.css';
+// ISOTOPE for Gallery
 // import '../../../node_modules/isotope-layout/dist/isotope.pkgd.min.js';
+// import '../../js/utils/isotope/fit-columns.js';
+// import '../../js/utils/isotope/photo31337.js';
+
+// Ligthbox for Gallery
+// import '../../js/utils/lightbox2/js/lightbox.js'
+
+
+
+
 // import '../../js/utils/isotopHelper';
 
 // FROM https://isotope.metafizzy.co/#install
